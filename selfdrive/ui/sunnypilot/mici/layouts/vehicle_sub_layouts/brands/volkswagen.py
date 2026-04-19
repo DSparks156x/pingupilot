6b17817bef0828827e955bcfb6c60c23eb7bb0de
Drew Sparks <drew.sparks156@gmail.com>
2026-04-19 05:24:57 -0600

    diag buttons no showy

--- selfdrive/ui/sunnypilot/mici/layouts/vehicle_sub_layouts/brands/volkswagen.py
+++ selfdrive/ui/sunnypilot/mici/layouts/vehicle_sub_layouts/brands/volkswagen.py
@@ -7,7 +7,7 @@ See the LICENSE.md file in the root directory for more details.
 from openpilot.selfdrive.ui.sunnypilot.mici.layouts.vehicle_sub_layouts.brands.base import BrandSettingsMici
 from openpilot.selfdrive.ui.ui_state import ui_state
 from openpilot.system.ui.lib.multilang import tr
-from openpilot.selfdrive.ui.mici.widgets.button import BigMultiParamToggle, BigParamControl
+from openpilot.selfdrive.ui.mici.widgets.button import BigMultiParamToggle, BigParamControl, BigButton
 from opendbc.car.volkswagen.values import VolkswagenFlags
 
 
@@ -32,6 +32,10 @@ class VolkswagenSettingsMici(BrandSettingsMici):
       "AlphaLongitudinalEnabled"
     )
 
+    self.diag_platform = BigButton(tr("Diag: Platform"), "", scroll=True)
+    self.diag_pq = BigButton(tr("Diag: Is PQ"), "", scroll=True)
+    self.diag_long = BigButton(tr("Diag: Is Long"), "", scroll=True)
+
     self.items = []
 
   def update_settings(self):
@@ -54,15 +58,24 @@ class VolkswagenSettingsMici(BrandSettingsMici):
       config = VW_CAR[platform].config
       is_pq = config.flags & VolkswagenFlags.PQ
       is_long_available = is_pq
+      diag_flags = hex(int(config.flags))
     elif ui_state.CP is not None:
       is_pq = ui_state.CP.flags & VolkswagenFlags.PQ
       is_long_available = ui_state.CP.alphaLongitudinalAvailable
+      diag_flags = hex(int(ui_state.CP.flags))
     elif platform in VW_CAR.__members__:
       config = VW_CAR[platform].config
       is_pq = config.flags & VolkswagenFlags.PQ
       is_long_available = is_pq
+      diag_flags = hex(int(config.flags))
+    else:
+      diag_flags = "None"
 
-    items = []
+    self.diag_platform.set_value(str(platform))
+    self.diag_pq.set_value(f"{bool(is_pq)} ({diag_flags})")
+    self.diag_long.set_value(str(is_long_available))
+
+    items = [self.diag_platform, self.diag_pq, self.diag_long]
     if is_pq:
       items.extend([
         self.hca_mode,
