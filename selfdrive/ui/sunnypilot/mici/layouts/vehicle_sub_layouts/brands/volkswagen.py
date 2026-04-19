e7aa8dae93122570347bd05bcef013578410d3a8
Drew Sparks <drew.sparks156@gmail.com>
2026-04-19 02:48:58 -0600

    more volkswagen

--- selfdrive/ui/sunnypilot/mici/layouts/vehicle_sub_layouts/brands/volkswagen.py
+++ selfdrive/ui/sunnypilot/mici/layouts/vehicle_sub_layouts/brands/volkswagen.py
@@ -5,8 +5,10 @@ This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
 from openpilot.selfdrive.ui.sunnypilot.mici.layouts.vehicle_sub_layouts.brands.base import BrandSettingsMici
+from openpilot.selfdrive.ui.ui_state import ui_state
 from openpilot.system.ui.lib.multilang import tr
-from openpilot.selfdrive.ui.mici.widgets.button import BigMultiParamToggle
+from openpilot.selfdrive.ui.mici.widgets.button import BigMultiParamToggle, BigParamControl
+from opendbc.car.volkswagen.values import VolkswagenFlags
 
 
 class VolkswagenSettingsMici(BrandSettingsMici):
@@ -25,10 +27,26 @@ class VolkswagenSettingsMici(BrandSettingsMici):
       [tr("±5"), tr("±10"), tr("±30"), tr("±50")]
     )
 
-    self.items = [
-      self.hca_mode,
-      self.hca_delta_rate,
-    ]
+    self.experimental_long = BigParamControl(
+      tr("Experimental Longitudinal"),
+      "AlphaLongitudinalEnabled"
+    )
+
+    self.items = []
 
   def update_settings(self):
-    pass
+    if ui_state.CP is None:
+      self.items = []
+      return
+
+    items = []
+    if ui_state.CP.flags & VolkswagenFlags.PQ:
+      items.extend([
+        self.hca_mode,
+        self.hca_delta_rate,
+      ])
+
+    if ui_state.CP.alphaLongitudinalAvailable:
+      items.append(self.experimental_long)
+
+    self.items = items
